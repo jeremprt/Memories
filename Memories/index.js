a9bb26aedef5d792582e98e01e381899e63cabf0
jeremprt <87941122+jeremprt@users.noreply.github.com>
2022-05-03 18:25:29 +0200

    Add files via upload

---
+++ Memories/index.js
@@ -0,0 +1,15 @@
+//tableaux image caché
+
+function HideCard()
+{
+    let TabCardHide = ["CardB"]
+   TabCardHide.onclick(TabCardHide.slice([0]))
+}
+
+HideCard()
+
+
+
+//fonction click devient transparent disparer
+
+//fonction push lautre img dans le tableau vide 
